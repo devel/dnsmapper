1fd7eeed0009cb3d0f6782d9f8b42ac99b553ea5
Ask Bjørn Hansen <ask@develooper.com>
2014-12-11 23:07:33 -0800

    Specify TLS ciphers

--- http.go
+++ http.go
@@ -190,7 +190,21 @@ func httpHandler() {
 
 		go func() {
 
-			tlsconfig := &tls.Config{MinVersion: tls.VersionTLS10}
+			tlsconfig := &tls.Config{
+				ClientSessionCache: tls.NewLRUClientSessionCache(100),
+				MinVersion:         tls.VersionTLS10,
+				CipherSuites: []uint16{
+					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+					tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+					tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+					tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+					tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA},
+			}
 
 			tlslisten := *flagip + ":" + *flaghttpsport
 			srv := &http.Server{
